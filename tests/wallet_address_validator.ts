58542dd83e46ae49c92a81486eed820c0b994aed
mark <mark.suurland@cryptobroker.ch>
2022-06-21 11:07:04 +0200

    bump some depens, and fix broken tests

--- tests/wallet_address_validator.ts
+++ tests/wallet_address_validator.ts
@@ -295,12 +295,12 @@ describe('validate', function () {
         });
 
         it('should return true for correct Binance Coin Mainnet addresses', function () {
-            valid('0xE37c0D48d68da5c5b14E5c1a9f1CFE802776D9FF', 'binance coin mainnet');
-            valid('0xa00354276d2fC74ee91e37D085d35748613f4748', 'binance coin mainnet');
-            valid('0xAff4d6793F584a473348EbA058deb8caad77a288', 'BNB');
-            valid('0xc6d9d2cd449a754c494264e1809c50e34d64562b', 'bnb');
-            valid('0x52908400098527886E0F7030069857D2E4169EE7', 'BNB');
-            valid('0x8617E340B3D01FA5F11F306F4090FD50E238070D', 'bnb');
+            valid('0xE37c0D48d68da5c5b14E5c1a9f1CFE802776D9FF', 'binance coin mainnet', null);
+            valid('0xa00354276d2fC74ee91e37D085d35748613f4748', 'binance coin mainnet', null);
+            valid('0xAff4d6793F584a473348EbA058deb8caad77a288', 'BNB', null);
+            valid('0xc6d9d2cd449a754c494264e1809c50e34d64562b', 'bnb', null);
+            valid('0x52908400098527886E0F7030069857D2E4169EE7', 'BNB', null);
+            valid('0x8617E340B3D01FA5F11F306F4090FD50E238070D', 'bnb', null);
         });
 
         it('should return true for correct Baby Ripple addresses', function () {
